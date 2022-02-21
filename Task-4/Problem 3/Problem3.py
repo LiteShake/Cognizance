5fe5731fc821e2829194f1be9bfe2f5dcf5a53f3
LiteShake <strawberryshakelite@gmail.com>
2022-02-21 17:34:58 +0530

    Update Problem3.py

--- Task-4/Problem 3/Problem3.py
+++ Task-4/Problem 3/Problem3.py
@@ -4,27 +4,31 @@ for instance, student details like Roll no, Student Name, Marks
 """
 
 # INITIALIZING ARRAY
+from audioop import add
+
+from numpy import array
+
+
 data2D = [
-    ["Roll No","Name","Marks"]
+    ["Item No","Name","Price"]
 ]
+
+
+# FUNCTION TO PRINT 1D ARRAY
+def Print1DArray( list1d ) :
+    for elemt in list1d :
+        print( str(elemt) + "        " , end = "")  
 
 # FUNCTION TO PRINT 2D ARRAY
 def Print2DArray( list2d ) :
 
     for elemt in list2d[0] :
-        print( elemt + "   ", end = " ")
+        print( elemt + "     ", end = " ")
     print()
 
     for list1d in list2d[1:] :
-
-        for elemt in list1d :
-            print( str(elemt) + "        ", end = " ")
+        Print1DArray( list1d )
         print()
-
-# FUNCTION TO PRINT 1D ARRAY
-def Print1DArray( list1d ) :
-    for elemt in list1d :
-        print( str(elemt) + "        ", end = " ")  
 
 
 """
@@ -33,11 +37,22 @@ i) Add some records in the list and print the list in tabular form,
 # ADDING REQUIRED ELEMENTS TO ARRAY
 def AddToArray() :
     
-    data2D.append( [1,"Abc",90] )
-    data2D.append( [2,"Def",95] )
-    data2D.append( [3,"Ghi",85] )
+    try :       # ANTI-CRASH IF USER GIVES STRING IN INT / FLOAT FIELDS
+        item_no = int(input("\nEnter item no : "))
+
+        if ( item_no == -1 ) : return False     # BREAKS LOOP IF item_no IS -1
+
+        item_name = input("Enter name : ")
+        price = float(input("Enter price : "))
+        print()
+
+        data2D.append( [item_no, item_name, price] )
+        
+        Print2DArray(data2D)
+        return True
     
-    Print2DArray(data2D)
+    except :
+        return True
 
 """
 ii) From created list, extract and print second record/row that contains
@@ -48,7 +63,10 @@ def PrintSecondRow() :
     Print1DArray( data2D[2] )
 
 # START HERE
-AddToArray()
+Print2DArray(data2D)
+
+arrayAdd = True
+while( arrayAdd ) : arrayAdd = AddToArray()     # ASK THE USER DATA UNTIL RETURN VALUE IS FALSE
 
 PrintSecondRow()
 
