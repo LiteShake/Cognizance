f7f491b2803b666865fd1f901099a1a5005344b6
LiteShake <strawberryshakelite@gmail.com>
2022-02-21 15:19:54 +0530

    Update Problem5.py

--- Task-4/Problem 5/Problem5.py
+++ Task-4/Problem 5/Problem5.py
@@ -7,8 +7,6 @@ Print the following pattern.
 * * * * *
 """
 
-
-
 def Pattern( layers ) :     # THIS A FUNCTION
 
     for layer in range( layers , 0, -1 ) :      # LOOPING IN REVERSE FROM NUMBER OF LAYERS TO 0 
