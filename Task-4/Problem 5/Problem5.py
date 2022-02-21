d773b4bd5f44a21a16640740f1d0483b4866f738
LiteShake <strawberryshakelite@gmail.com>
2022-02-21 15:15:12 +0530

    Reorganized readme files

--- Task-4/Problem 5/Problem5.py
+++ Task-4/Problem 5/Problem5.py
@@ -6,14 +6,7 @@ Print the following pattern.
  * * * *
 * * * * *
 """
-"""
-Quick intro :
 
-After seeing the pattern I saw that the number of  stars are increasing according to the layer number and 
-the number of spaces are decreasing from (layer number - 1) to 0.
-So I put that thing in a for loop and multiplied the string to get repeated string accordingly,
-concatenated and printed one by one.
-"""
 
 
 def Pattern( layers ) :     # THIS A FUNCTION
---
+++ Task-4/Problem 5/readme.md
@@ -0,0 +1,6 @@
+<h2> Quick intro : </h2>
+
+- After seeing the pattern I saw that the number of  stars are increasing according to the layer number and 
+the number of spaces are decreasing from (layer number - 1) to 0.
+- So I put that thing in a for loop and multiplied the string to get repeated string accordingly,
+concatenated and printed one by one.
